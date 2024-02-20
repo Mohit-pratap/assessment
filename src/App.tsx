cc667a7d9789fb3250df04a98f0df4d5b51055b3
mohit.pratap <mohit.pratap@okoders.com>
2024-02-20 21:02:02 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import necessary modules
+import * as React from 'react';
+import { Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home/Home';
+import NotFound from './Components/Pages/NotFound/NotFound';
 
-function App() {
+// Define the component that renders routes
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
